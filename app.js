343994262d58d44f2a3a6b54fde74abed3bb45bf
Trang Nguyen <trangnguyen.281198@gmail.com>
2020-01-17 15:07:09 +0300

    Change for development to heroku

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(helmet());
 
 // Setup mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://tracy:nguyen@cluster0-or7z7.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://tracy:nguyen@cluster0-or7z7.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
