cb4a2730e30635af3687cbe7e003ef71067fe6c8
Trang Nguyen <trangnguyen.281198@gmail.com>
2020-01-17 18:30:11 +0300

    Add security config

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var app = express();
 app.use(helmet());
 
 // Setup mongoose connection
+require('dotenv').config();
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://tracy:nguyen@cluster0-or7z7.mongodb.net/test?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
